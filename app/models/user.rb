f6f4f856812dcc2a2e9dbe17e0e2bc6b528df647
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-14 10:15:57 -0500

    Friends association is working in both directions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   has_many :friendeeships, class_name: "Friendship", foreign_key: "friendee_id"
   has_many :frienders, through: :friendeeships, source: :friender
   has_many :friendees, through: :frienderships, source: :friendee
+
+  def friends
+    output = []
+    output.concat(frienders)
+    output.concat(friendees)
+    output
+  end
+
+
 end
