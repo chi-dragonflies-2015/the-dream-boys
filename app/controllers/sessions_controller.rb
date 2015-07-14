cef288209769fd3d9e8cbb29c1b93028d8b82040
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-14 16:39:20 -0500

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: session_params[:id])
-    if @user.authenticate(session_params[:password])
+    @user = User.find_by(username: session_params[:username])
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to "/"
     else
