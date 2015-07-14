d74bfca78b5f7a8adb1c42a605ac6084aef7c92f
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-14 10:30:32 -0500

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   has_many :frienders, through: :friendeeships, source: :friender
   has_many :friendees, through: :frienderships, source: :friendee
 
+  has_secure_password
+
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, presence: true
+
+
   def friends
     output = []
     output.concat(frienders)
--- db/migrate/20150714142720_create_users.rb
+++ db/migrate/20150714142720_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username#, null: false
-      t.string :username#, null: false
-      t.string :username#, null: false
+      t.string :first_name#, null: false
+      t.string :last_name#, null: false
       t.string :password_digest#, null: false
       t.string :bio
-      t.boolean :admin
+      t.boolean :admin, default: false
     end
   end
 end
