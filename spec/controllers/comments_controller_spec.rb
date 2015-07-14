707518ab4708c4f5717de3766ae5994a9dfabb44
boguth <bo.guthrie.mail@gmail.com>
2015-07-14 18:29:55 -0500

    Returning controller tests for the show and index routes.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,15 +3,20 @@ require 'rails_helper'
 RSpec.describe CommentsController, :type => :controller do
   describe 'GET index' do
     it "returns a 302 status code" do
-      # comment = Game.find_by(id: 1).comments.new
       get :index, game_id: 1
       expect(response.status).to eq(302)
     end
   end
 
+   describe 'POST create' do
+    it "returns a 302 status code" do
+      post :create
+      expect(response.status).to eq(200)
+    end
+  end
+
   describe 'GET show' do
     it "returns a 302 status code" do
-      # comment = Game.find_by(id: 1).comments.new
       get :show, game_id: 1, id: 1
       expect(response.status).to eq(302)
     end
