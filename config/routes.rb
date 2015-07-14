bc190befaa15a5578a1f49441e6754e5d2a1aa60
boguth <bo.guthrie.mail@gmail.com>
2015-07-14 14:38:12 -0500

    Committing routes - adding nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  resourses :games do
+  resources :games do
     resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
