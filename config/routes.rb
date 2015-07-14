de3a022159cab09783c2b3fb8ab8dbfdca036aea
boguth <bo.guthrie.mail@gmail.com>
2015-07-14 16:40:47 -0500

    Updated routes to reflect better nesting of comments within both users and games.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   resources :games do
     resources :comments
   end
+
   get 'games/:game_id/vote/:vote_value' => 'vote#create_vote_for_game'
   get 'comments/:comment_id/vote/:vote_value' => 'vote#create_vote_for_comment'
 
-  resources :users
+  resources :users do
+    resources :comments
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
 end
