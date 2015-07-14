7f40b7866ead613a5633d79fd050d584be5d57cb
Bo Guthrie & Michael Kunc <pair+boguth.michaelkunc@devbootcamp.com>
2015-07-14 10:18:23 -0500

    custom validations complete. game seed is working

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,11 @@
-class Games < ActiveRecord::Base
+class Game < ActiveRecord::Base
   has_many :comments
   has_many :games_tags
   has_many :tags, through: :games_tags
   has_many :ownerships
   has_many :owners, through: :ownerships, source: :owner_id
   has_many :votes, as: :voteable
-  has_many :votes, though: :votes, source: :user
+  has_many :votes, through: :votes, source: :user
 
   validates :title, {presence: true, uniqueness: true}
   validates :description, {presence: true}
@@ -14,8 +14,8 @@ class Games < ActiveRecord::Base
   validates :min_age, {presence: true}
   validates :min_time, {presence: true}
   validates :max_time, {presence: true}
-  validates :min_player_less_than_max
-  validates :min_time_less_than_max
+  validate  :min_player_less_than_max
+  validate  :min_time_less_than_max
 
   private
 
@@ -28,6 +28,7 @@ class Games < ActiveRecord::Base
   def min_time_less_than_max
     if self.min_time > self.max_time
       errors.add(:min_time, "should be less than max time.")
+    end
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+game = Game.create({title: "Battleship",
+                  image_url: "http://placehold.it/200x100",
+                  description: "The original naval game",
+                  min_players: 2,
+                  max_players: 2,
+                  min_age: 10,
+                  min_time: 10,
+                  max_time: 25} )
