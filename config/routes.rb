529b841268769615b7ac34f629d588bf28cd6bc7
Bo Guthrie & Michael Kunc <pair+boguth.michaelkunc@devbootcamp.com>
2015-07-14 16:25:48 -0500

    links on global game page. start of game profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
-
-  resources :games do
-    resources :comments
-  end
-  get 'games/:game_id/vote/:vote_value' => 'vote#create_vote_for_game'
-  get 'comments/:comment_id/vote/:vote_value' => 'vote#create_vote_for_comment'
+  resources :games
+  root "home#index"
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
