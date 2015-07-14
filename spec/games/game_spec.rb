e23fa43e389a286b414de5494cd030c78eab3b0e
Bo Guthrie & Michael Kunc <pair+boguth.michaelkunc@devbootcamp.com>
2015-07-14 11:15:26 -0500

    first validation test is working in games

--- spec/games/game_spec.rb
+++ spec/games/game_spec.rb
@@ -1,8 +1,25 @@
+require 'rails_helper'
 require 'spec_helper'
 
-describe "Game" do
-  it "should have a name" do
+describe Game do
 
+  describe "validations" do
+
+    before (:each) do
+      @game = Game.new({title: "Battleship",
+                          image_url: "http://placehold.it/200x100",
+                          description: "The original naval game",
+                          min_players: 2,
+                          max_players: 2,
+                          min_age: 10,
+                          min_time: 10,
+                          max_time: 25} )
     end
 
+      it "should be invalid witout a name" do
+        @game.title = ""
+        expect(@game.save).to eq(false)
+    end
+
+  end
 end
