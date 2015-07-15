3dd9533655f920f82f6609e93b5853385c1edc7a
Nick Siefken <nicks720@gmail.com>
2015-07-14 19:29:38 -0500

    create tags and join table between tags and games

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :games do
     resources :comments
   end
-  get 'games/:game_id/vote/:vote_value' => 'vote#create_vote_for_game'
-  get 'comments/:comment_id/vote/:vote_value' => 'vote#create_vote_for_comment'
+  post 'games/:game_id/vote/:vote_value' => 'vote#create_vote_for_game'
+  post 'comments/:comment_id/vote/:vote_value' => 'vote#create_vote_for_comment'
 
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714190940) do
+ActiveRecord::Schema.define(version: 20150714235401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20150714190940) do
     t.integer  "games_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
