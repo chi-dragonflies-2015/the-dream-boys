afff3c487812b280f6f6570707da178fb85c1919
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-15 09:43:38 -0500

    add schema to gitignore and remove extraneous resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   post 'games/:game_id/vote/:vote_value' => 'vote#create_vote_for_game'
   post 'comments/:comment_id/vote/:vote_value' => 'vote#create_vote_for_comment'
 
-  resources :users
-
   resources :users do
     resources :comments
   end
